99ce4555a15797031e60a3c2106d0e55e066650e
Peter Backman <ptr.bckmn@gmail.com>
2010-08-01 16:47:11 +0200

    n/a

--- new_api.rb
+++ new_api.rb
@@ -1,8 +1,23 @@
-my_window = Window.new {
+
+# 1 ----------------------------------------------------------------------------
+main_window = Window.new {
 	title "My Window, eh!"
 	
 	button1 = Button.new {
 		title "A button"
+		on_click {create_document_window "boo"}
 	}
 }
+
+
+
+def create_document_window(name)
+	Window.new {
+		title "Document: " + name
+		
+		text = TextArea.new {
+			...
+		}
+	}
+end
 
