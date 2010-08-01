78c2a50d0422e0422adffc9579832b0dfd4c31a1
Peter Backman <ptr.bckmn@gmail.com>
2010-08-01 18:12:48 +0200

    working on the api proposal

--- new_api.rb
+++ new_api.rb
@@ -5,15 +5,19 @@ main_window = Window.new {
 	
   button1 = Button.new {
     title "A button"
-    on_click {create_document_window "boo"}
+    on_click {document_window "boo"}
   }
+  
+  print = print_button
 }
 
 main_window.button1.on_terminate {
   puts "Bye!"
 }
 
-def create_document_window(name)
+
+
+def document_window(name)
   Window.new {
     title "Document: " + name
 		
@@ -22,4 +26,19 @@ def create_document_window(name)
     }
   }
 end
+
+def print_button
+  Button.new {
+    title "Print!"
+    on_click {
+      document = get_doc;
+      document.format
+      printer = get_printer;
+      printer.print document
+    }
+  }
+end
+
+
+
 
